ead4b2a750b9d3aff24bac7c49e075bc5df04863
rahmaelwazzan <105241522+rahmaelwazzan@users.noreply.github.com>
2023-01-14 19:15:21 +0200

    Update 19318_code.ino

--- 19318_code.ino
+++ 19318_code.ino
@@ -1,52 +1,64 @@
+#include <ESP32Servo.h>
+Servo myservo;
 
 #include <WiFi.h>
 #include "secrets.h"
 #include "ThingSpeak.h"
 #include "DHT.h"
-#define DHTPIN 5
-#define DHTPIN2 18
-
+#define DHTPIN 18
+#define DHTPIN2 5
+#include <stdio.h>
+#include <stdlib.h>
+int servoPin = 19;
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
 
-
-
-
-int sensorValue, gas, gasDiff, co2, h2o , mashy;
+int sensorValue, gas, gasDiff, co2, h2o, moles;
 int oldGas = 0;
 int counter = 0;
 int done = 0;
 float h, t, f, h2, t2, f2, tempDiff, tempDiff1, tempDiff2;
 float temp1 = 0;
 float temp2 = 0;
-//String txt1 = "As h2o levels increased by.236 ppm, relative humidity was found to elevate by 42%";
-char *txt1[] = {"As co2 increases by 425 ppm, it participates in trapping sunlight in the greanhouse. And thus, it elevates temperature levels by 1 degree Celsius.",
-                "As h2o levels increased by 346 ppm, relative humidity was found to elevate by 38 percent"
-               };
-char ssid[] = SECRET_SSID;   // your network SSID (name)
-char pass[] = SECRET_PASS;   // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-WiFiClient  client;
+
+char *txt1[300];
+
+char ssid[] = SECRET_SSID;  // your network SSID (name)
+char pass[] = SECRET_PASS;  // your network password
+int keyIndex = 0;           // your network key Index number (needed only for WEP)
+WiFiClient client;
 
 unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+const char *myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+int co2Val, tempVal, h2oVal, humVal;
+char snum[10];
 
 // Initialize our values
 //String txt2 = "As h2o levels increased by.....ppm, relative humidity was found to elevate by ...%";
 
 void setup() {
-  Serial.begin(9600);  //Initialize serial
+  pinMode(21, OUTPUT);
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);            // standard 50 hz servo
+  myservo.attach(servoPin, 1000, 2000);  // attaches the servo on pin 18 to the servo object
+  myservo.attach(9);
+  Serial.begin(115200);  //Initialize serial
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+    ;  // wait for serial port to connect. Needed for Leonardo native USB port only
   }
+  servoClose();
 
   WiFi.mode(WIFI_STA);
   ThingSpeak.begin(client);
 
 
   Serial.println(F("DHTxx test!"));
-  dht.begin();
+  dht.begin();  
   dht2.begin();
   while (millis() < 2000) {
     dht2Reading();
@@ -58,7 +70,7 @@ void setup() {
     Serial.print(t);
     temp2 += t2;
     Serial.print("\t temp2= ");
-    Serial.println(t);
+    Serial.println(t2);
     gas += sensorValue;
     Serial.println();
     delay(600);
@@ -73,10 +85,23 @@ void setup() {
   Serial.print("\t Gas average : ");
   Serial.println(gas);
   Serial.println();
-  delay(1000);
+  delay(20000);
+  servoOpen();
+  digitalWrite(21, OUTPUT);
 }
 void loop() {
   //fullcode
+  char txt[500] = "As co2 increases by ";
+  //co2Val
+  char txt2[] = " ppm, it participates in trapping sunlight in the greanhouse. And thus, it elevates temperature levels by ";
+  //tempVal
+  char txt3[] = " As h2o levels increased by  ";
+  //h2oVal
+  char txt4[] = "  ppm relative humidity was found to elevate by ";
+  //humVal
+
+  // convert 123 to string [buf]
+
   dht2Reading();
   dhtReading();
 
@@ -100,20 +125,20 @@ void loop() {
   Serial.print("Final Temrature difference = ");
   Serial.print(tempDiff);
   Serial.println(" °C");
-  gasDiff = gas - oldGas ;
+  gasDiff = gas - oldGas;
   if (gasDiff < 0)
     gasDiff *= -1;
-  Serial.print( "Gass Diff= " );
-  Serial.println( gasDiff );
-  mashy = gasDiff / 11;
-  co2 = mashy * 6;
-  Serial.print( "co2=" );
-  Serial.print( co2 );
-  Serial.println ( " PPM");
-  h2o = mashy * 5;
-  Serial.print( "h2o=" );
-  Serial.print( h2o );
-  Serial.println ( "PPM");
+  Serial.print("Gass Diff= ");
+  Serial.println(gasDiff);
+  moles = gasDiff / 11;
+  co2 = moles * 6;
+  Serial.print("co2=");
+  Serial.print(co2);
+  Serial.println(" PPM");
+  h2o = moles * 5;
+  Serial.print("h2o=");
+  Serial.print(h2o);
+  Serial.println("PPM");
   oldGas = sensorValue;
   gasReading();
 
@@ -129,6 +154,42 @@ void loop() {
     }
     Serial.println("\nConnected.");
   }
+  co2Val = abs(co2 - co2Val);
+  co2Val = co2;
+  Serial.print("co2Val=");
+  Serial.println(co2Val);
+
+  itoa(co2Val, snum, 10);
+  strncat(txt, snum, 10);
+
+  strncat(txt, txt2, 200);
+  // Serial.print("tempDiff= ");
+  // Serial.println(tempDiff);
+  tempVal = abs(tempDiff - tempVal);
+  // Serial.print("tempValold=");
+  // Serial.println(tempVal);
+
+  tempVal = tempDiff;
+
+  // Serial.print("tempVal=");
+  // Serial.println(tempVal);
+
+  itoa(tempVal, snum, 10);
+  strncat(txt, snum, 10);
+
+  strncat(txt, txt3, 200);
+
+  h2oVal = abs(h2o - h2oVal);
+  h2oVal = h2o;
+  itoa(h2oVal, snum, 10);
+  strncat(txt, snum, 10);
+
+  strncat(txt, txt4, 200);
+
+  humVal = abs(h - humVal);
+  humVal = h;
+  itoa(humVal, snum, 10);
+  strncat(txt, snum, 10);
 
   // set the fields with the values
   ThingSpeak.setField(1, tempDiff);
@@ -136,7 +197,7 @@ void loop() {
   ThingSpeak.setField(3, co2);
   ThingSpeak.setField(4, h2o);
 
-  ThingSpeak.setStatus(txt1[1]);
+  ThingSpeak.setStatus(txt);
 
 
   // figure out the status message
@@ -147,12 +208,11 @@ void loop() {
   int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
   if (x == 200) {
     Serial.println("Channel update successful.");
-  }
-  else {
+  } else {
     Serial.println("Problem updating channel. HTTP error code " + String(x));
   }
   // change the values
-  delay(5000); // Wait 20 seconds to update the channel again
+  delay(5000);  // Wait 20 seconds to update the channel again
 }
 
 void dhtReading() {
@@ -172,36 +232,38 @@ void dhtReading() {
   Serial.println(F(" °C "));
   Serial.println();
   delay(100);
-
 }
 void dht2Reading() {
   h2 = dht2.readHumidity();
   t2 = dht2.readTemperature();
   f2 = dht2.readTemperature(true);
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h2) || isnan(t2) || isnan(f2)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-  float hif = dht.computeHeatIndex(f, h);
+  float hif = dht2.computeHeatIndex(f2, h2);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  float hic = dht2.computeHeatIndex(t2, h2, false);
 
   //Serial.print(F("Humidity: "));
   //Serial.print(h);
   Serial.print(F("Temperature2: "));
-  Serial.print(t, 1);
+  Serial.print(t2, 1);
   Serial.println(F(" °C "));
   Serial.println();
   delay(100);
-
 }
-void gasReading()
-{
+void gasReading() {
   sensorValue = analogRead(36);
   Serial.print("AirQua=");
   Serial.print(sensorValue, DEC);
   Serial.println(" PPM");
   Serial.println();
   delay(100);
-
+}
+void servoOpen() {
+  myservo.write(0);  // tell servo to go to position in variable 'pos'
+}
+void servoClose() {
+  myservo.write(90);  // sets the servo position according to the scaled value
 }
